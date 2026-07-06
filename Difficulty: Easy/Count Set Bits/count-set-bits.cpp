3e8dadd200513cadcbec2d258f3d25a77650e3f3
Kunal Yelgate <kunalyelgatew@gmail.com>
2026-07-06 10:44:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Set Bits/count-set-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int setBits(int n) {
+         int count = 0;
+         while(n) {
+                n = n & ( n - 1);
+                count++;
+         }
+        return count;
+    }
+};
+
